6a3a0b52b8cbafc4d0291b8f566552105ebe3722
DEVQuang <250412quang@gmail.com>
2024-08-18 19:58:02 +0700

    Add project files.

---
+++ HelloWord/Controllers/HelloWord.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWord.Controllers
+{
+    public class HelloWord : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HelloWord/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloWord.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
